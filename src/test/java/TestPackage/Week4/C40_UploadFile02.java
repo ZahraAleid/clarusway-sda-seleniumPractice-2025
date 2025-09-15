1a4becd4dec8a1a8a5ed8375cd8f6b8bcfd5d590
ZahraAleid <zahra.eid112@gmail.com>
2025-09-15 13:53:22 +0300

    add file info and download/upload tests for Selenium interactions

---
+++ src/test/java/TestPackage/Week4/C40_UploadFile02.java
@@ -0,0 +1,18 @@
+package TestPackage.Week4;
+import org.junit.jupiter.api.Test;
+import utilities.TestBase;
+
+public class C40_UploadFile02 extends TestBase {
+/*
+    Go to URL: https://testpages.eviltester.com/styled/basic-html-form-test.html
+    Find the path of the file that you want to upload.
+    Click on Upload button.
+    Verify the upload message.
+*/
+
+    @Test
+    public void uploadTest() {
+
+    }
+
+}
